5dca61454f684daecc23a3c124caa432de0d58e6
ppparkta <pppppark2@gmail.com>
2024-08-17 21:20:02 +0900

    data/#4: 디렉토리 정리 & 배포 준비

--- app/main.py
+++ app/main.py
@@ -3,16 +3,19 @@ import tensorflow as tf
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
-
+from mangum import Mangum
 from sklearn.preprocessing import StandardScaler
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 app = FastAPI()
 
-# 미리 학습된 모델 로드
-model = tf.keras.models.load_model('model_1.keras')  # 모델 경로를 적절히 변경
+# TensorFlow Lite 모델 로드
+interpreter = tf.lite.Interpreter(model_path="./static/LSTM_model/model_1.tflite")
+interpreter.allocate_tensors()
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
-# Pydantic 모델 정의
+
 class SensorData(BaseModel):
     accX: float
     accY: float
@@ -21,30 +24,35 @@ class SensorData(BaseModel):
     gyroY: float
     gyroZ: float
 
+
 class RequestBody(BaseModel):
     fall: List[SensorData]
 
 def preprocess_input(data: List[SensorData]):
+    # 데이터 전처리 (정규화 포함)
     data = np.array([[item.accX, item.accY, item.accZ, item.gyroX, item.gyroY, item.gyroZ] for item in data])
-    
+
     scaler = StandardScaler()
     data = scaler.fit_transform(data)
-    
-    max_sequence_length = 100 
+
+    max_sequence_length = 100
     padded_data = pad_sequences([data], maxlen=max_sequence_length, padding='post', dtype='float32')
-    
+
     return padded_data
+
+@app.get("/")
+def read_root():
+    return {"Message": "Hello World"}
 
 @app.post("/api/ai/predict")
 async def predict(item: RequestBody):
     try:
-        # 입력 데이터를 전처리
         input_data = preprocess_input(item.fall)
 
-        # 모델로 예측 수행
-        prediction = model.predict(input_data)
+        interpreter.set_tensor(input_details[0]['index'], input_data)
+        interpreter.invoke()
+        prediction = interpreter.get_tensor(output_details[0]['index'])
 
-        # 예측 결과를 이진 분류 (True/False)로 변환
         result = bool(prediction[0] > 0.5)
 
         return {"result": result}
@@ -52,6 +60,4 @@ async def predict(item: RequestBody):
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+handler = Mangum(app)
